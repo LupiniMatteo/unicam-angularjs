b9edb57206d62e0669421a97af9c6331be47c0b9
Saverio Tosi <s.tosi@extrategy.net>
2017-04-29 17:37:00 +0200

    fix student detail component

--- app/student-detail/student-detail.component.js
+++ app/student-detail/student-detail.component.js
@@ -1,12 +1,12 @@
 // Register the `studentDetail` component on the `studentDetail` module,
 angular.module('studentDetail').component('studentDetail', {
   templateUrl: 'student-detail/student-detail.template.html',
-  controller: function ($http) {
+  controller: function ($http, $routeParams) {
     var studentDetail = this;
-    studentDetail.orderProp = 'name';
+    studentDetail.studentId = $routeParams.studentId;
 
-    $http.get('api/v1.0/students').then(function(response) {
-      studentDetail.students = response.data;
+    $http.get('api/v1.0/students/' + studentDetail.studentId).then(function(response) {
+      studentDetail.student = response.data;
     });
   }
 });
