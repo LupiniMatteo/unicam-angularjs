038923c9b17572caf31e8ece2e840530d11ba6bc
Saverio Tosi <s.tosi@extrategy.net>
2017-04-29 17:00:53 +0200

    aggiunto un nuovo test

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -3,14 +3,14 @@
 // Angular E2E Testing Guide:
 // https://docs.angularjs.org/guide/e2e-testing
 
-describe('Student Application', function() {
+describe('Student Application', function () {
 
-  describe('phoneList', function() {
-    beforeEach(function() {
+  describe('phoneList', function () {
+    beforeEach(function () {
       browser.get('index.html');
     });
 
-    it('should filter the student list as a user types into the search box', function() {
+    it('should filter the student list as a user types into the search box', function () {
       var studentList = element.all(by.repeater('student in $ctrl.students'));
       var query = element(by.model('$ctrl.query'));
 
@@ -24,14 +24,23 @@ describe('Student Application', function() {
       expect(studentList.count()).toBe(3);
     });
 
-    it('should be possible to control student order via the drop-down menu', function() {
+    it('should render student specific links', function () {
+      var query = element(by.model('$ctrl.query'));
+      query.sendKeys('saverio');
+
+      element.all(by.css('.students li a')).first().click();
+      expect(browser.getLocationAbsUrl()).toBe('/students/590364b7babf803c177f111c');
+    });
+
+
+    it('should be possible to control student order via the drop-down menu', function () {
       var queryField = element(by.model('$ctrl.query'));
       var orderSelect = element(by.model('$ctrl.orderProp'));
       var nameOption = orderSelect.element(by.css('option[value="CFU"]'));
       var studentNameColumn = element.all(by.repeater('student in $ctrl.students').column('student.name'));
 
       function getNames() {
-        return studentNameColumn.map(function(elem) {
+        return studentNameColumn.map(function (elem) {
           return elem.getText();
         });
       }
