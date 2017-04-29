dbd0f862827f924d0c987611b2b62dfa26b05640
Saverio Tosi <s.tosi@extrategy.net>
2017-04-29 15:28:54 +0200

    aggiunti teste e2e

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -3,11 +3,26 @@
 // Angular E2E Testing Guide:
 // https://docs.angularjs.org/guide/e2e-testing
 
-describe('PhoneCat Application', function() {
+describe('Student Application', function() {
 
-  it('true should be true', function() {
-    expect(true).toBe(true);
+  describe('phoneList', function() {
+    beforeEach(function() {
+      browser.get('index.html');
+    });
+
+    it('should filter the student list as a user types into the search box', function() {
+      var studentList = element.all(by.repeater('student in $ctrl.students'));
+      var query = element(by.model('$ctrl.query'));
+
+      expect(studentList.count()).toBe(5);
+
+      query.sendKeys('saverio');
+      expect(studentList.count()).toBe(1);
+
+      query.clear();
+      query.sendKeys('a');
+      expect(studentList.count()).toBe(3);
+    });
   });
-
 
 });
