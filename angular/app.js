e4ef397f93fc964ff23d54bd51e8d4701ebebd34
Saverio Tosi <s.tosi@extrategy.net>
2017-04-29 12:35:47 +0200

    aggiunto app.js

---
+++ angular/app.js
@@ -0,0 +1,8 @@
+// Define the `studentApp` module
+var studentApp = angular.module('studentApp', []);
+
+// Define the `PhoneListController` controller on the `studentApp` module
+studentApp.controller('StudentListController', function StudentListController() {
+  var studentList = this;
+  studentList.students = ['Paolo', 'Caio'];
+});
