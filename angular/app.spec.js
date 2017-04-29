454734c099b56f61b532c4eceebe2e3d5c60cc8d
Saverio Tosi <s.tosi@extrategy.net>
2017-04-29 14:12:06 +0200

    aggiornati i test

--- angular/app.spec.js
+++ angular/app.spec.js
@@ -2,9 +2,4 @@ describe('StudentListController', function() {
 
   beforeEach(function() {module('studentApp') });
 
-  it('should create a `students` model with 3 students', inject(function($controller) {
-    var ctrl = $controller('StudentListController', {});
-    expect(ctrl.students.length).toBe(5);
-  }));
-
 });
---
+++ angular/student-list.component.spec.js
@@ -0,0 +1,17 @@
+describe('studentList', function() {
+
+  // Load the module that contains the `studentList` component before each test
+  beforeEach(module('studentApp'));
+
+  // Test the controller
+  describe('StudentListController', function() {
+
+    it('should create a `students` model with 5 students', inject(function($componentController) {
+      var ctrl = $componentController('studentList');
+
+      expect(ctrl.students.length).toBe(5);
+    }));
+
+  });
+
+});
