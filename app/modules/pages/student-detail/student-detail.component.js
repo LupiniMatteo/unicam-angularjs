9f49c83899ff0cf9cf706f873c4e5360de6e10f7
Saverio Tosi <s.tosi@extrategy.net>
2017-04-30 15:22:28 +0200

    refactoring

--- app/modules/pages/student-detail/student-detail.component.js
+++ app/modules/pages/student-detail/student-detail.component.js
@@ -1,6 +1,6 @@
 // Register the `studentDetail` component on the `studentDetail` module,
 angular.module('studentDetail').component('studentDetail', {
-  templateUrl: 'student-detail/student-detail.template.html',
+  templateUrl: 'modules/pages/student-detail/student-detail.template.html',
   controller: function ($http, $routeParams) {
     var studentDetail = this;
     studentDetail.studentId = $routeParams.studentId;
--- app/modules/pages/student-list/student-list.component.js
+++ app/modules/pages/student-list/student-list.component.js
@@ -1,6 +1,6 @@
 // Register the `studentList` component on the `studentList` module,
 angular.module('studentList').component('studentList', {
-  templateUrl: 'student-list/student-list.template.html',
+  templateUrl: 'modules/pages/student-list/student-list.template.html',
   controller: function ($http) {
     var studentList = this;
     studentList.orderProp = 'name';
